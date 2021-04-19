4d32d183bd39300526eb7eceb986ff5ab8f6709c
Greg Brewton <gbrewton1980@gmail.com>
2021-04-18 20:28:22 -0600

    Basic layour of Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
+// Inline styles used for placeholder styling - to be replaced with styled components
 import React from "react";
+import { Link } from "react-router-dom";
 
 const sampleImg = require("../assets/sampleImg.png");
+const pizzaImg = require("../assets/Pizza.jpg");
+
 const SampleListing = (props) => (
   <div
     style={{
@@ -21,34 +25,106 @@ const SampleListing = (props) => (
     />
     <h3>{props.name}</h3>
     <p>{props.text}</p>
+    <div style={{ display: "flex" }} className="cta">
+      <span
+        style={{
+          marginRight: "2em",
+          border: "1px solid black",
+          borderRadius: "3px",
+          padding: "3px",
+        }}
+      >
+        distance
+      </span>
+      <span
+        style={{
+          marginRight: "2em",
+          border: "1px solid black",
+          borderRadius: "3px",
+          padding: "3px",
+        }}
+      >
+        delivery fee
+      </span>
+    </div>
   </div>
 );
 
 const Home = (props) => {
   return (
     <div id="home">
-      <header>
-        <h1>Lambda Eats</h1>
+      <header style={{ display: "flex", justifyContent: "space-between" }}>
+        <h1 style={{ marginLeft: "2em" }}>Lambda Eats</h1>
         <nav>
-          <ul>
-            <li>Home</li>
-            <li>Help</li>
+          <ul
+            style={{
+              listStyleType: "none",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <li
+              style={{
+                padding: "3px 5px",
+                marginRight: "1em",
+                border: "1px solid black",
+                borderRadius: "3px",
+              }}
+            >
+              Home
+            </li>
+            <li
+              style={{
+                padding: "3px 5px",
+                marginRight: "1em",
+                border: "1px solid black",
+                borderRadius: "3px",
+              }}
+            >
+              Help
+            </li>
           </ul>
         </nav>
       </header>
       <div
         style={{
           width: "100%",
+          height: "clamp(200px, 40vh, 600px)",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
+          backgroundImage: `url(${pizzaImg})`,
         }}
         className="banner"
       >
-        <p>Banner goes here</p>
+        <div
+          style={{
+            display: "inline-flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          className="overlay"
+        >
+          <p style={{ color: "white" }}>
+            Your favorite food, delivered while coding
+          </p>
+          <Link
+            style={{
+              border: "1px solid black",
+              borderRadius: "3px",
+              padding: "8px",
+              textDecoration: "none",
+              color: "black",
+            }}
+            to="/pizza"
+          >
+            Pizza?
+          </Link>
+        </div>
       </div>
       <main>
-        <h2>Food Delivery in Gotham City</h2>
+        <h2 style={{ marginLeft: "2em" }}>Food Delivery in Gotham City</h2>
         <div style={{ display: "flex" }} className="shopList">
           <SampleListing
             name="Jimmy's"
