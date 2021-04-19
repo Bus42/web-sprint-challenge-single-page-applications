2e00fc44e6f502d773f6efe7b4037965107333ff
Greg Brewton <gbrewton1980@gmail.com>
2021-04-18 21:37:42 -0600

    Adds checkboxes for toppings

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,17 +4,55 @@ const Form = (props) => {
   const initialFormData = {
     size: "",
     sauce: "",
-    toppings: [],
+    toppings: {
+      pepperoni: false,
+      pineapple: false,
+      olives: false,
+      bacon: false,
+      sausage: false,
+      hamburger: false,
+      chicken: false,
+      cheddar: false,
+      mozzarella: false,
+      sardines: false,
+      mushrooms: false,
+      onions: false,
+      peppers: false,
+      ham: false,
+    },
     glutenFree: false,
     specialInstructions: "",
     quantity: 1,
   };
+  const initialToppings = {
+    pepperoni: false,
+    pineapple: false,
+    olives: false,
+    bacon: false,
+    sausage: false,
+    hamburger: false,
+    chicken: false,
+    cheddar: false,
+    mozzarella: false,
+    sardines: false,
+    mushrooms: false,
+    onions: false,
+    peppers: false,
+    ham: false,
+  };
   const [formData, setFormData] = useState(initialFormData);
   const handleChange = (e) => {
-      const {name, value, checked} = e.target;
-      const val = value ? value : checked;
-      setFormData({...formData, [name]: val})
-  }
+    const { name, value, checked } = e.target;
+    const val = value ? value : checked;
+    setFormData({ ...formData, [name]: val });
+  };
+  const handleToppings = (e) => {
+    const { name, checked } = e.target;
+    const selectedToppings = { ...formData.toppings, [name]: checked };
+    const newFormData = { ...formData };
+    newFormData.toppings = selectedToppings;
+    setFormData(newFormData);
+  };
   return (
     <form
       style={{
@@ -22,73 +60,236 @@ const Form = (props) => {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "baseline",
-        width: 'clamp(400px, 35vw, 800px)'
+        width: "clamp(400px, 35vw, 800px)",
       }}
       action="submit"
     >
-      <label htmlFor="size">
-        <div className="section">
-          <p>Choice of size</p>
-          <small>required</small>
-        </div>
-        <select name="size" id="size" onChange={handleChange} >
+      {/* size dropdown */}
+      <div
+        style={{ display: "flex", flexDirection: "column" }}
+        className="sizeWrapper"
+      >
+        <label htmlFor="size">Choice of Size</label>
+        <small>required</small>
+        <select name="size" id="size" onChange={handleChange}>
           <option value={null}>Select a size</option>
           <option value="sm">Small - 8"</option>
           <option value="md">Medium - 12"</option>
           <option value="lg">Large - 14"</option>
           <option value="xl">Super - 18"</option>
         </select>
-      </label>
-      <div style={{display: 'inline-flex', flexDirection: 'column', alignItems: 'baseline'}} className="radioWrapper">
+      </div>
+      {/* radio buttons */}
+      <div
+        style={{
+          display: "inline-flex",
+          flexDirection: "column",
+          alignItems: "baseline",
+        }}
+        className="radioWrapper"
+      >
         <div className="radio">
           <input
-          onChange={handleChange}
+            onChange={handleChange}
             type="radio"
             name="sauce"
             value="marinara"
-            checked={formData.sauce === 'marinara'}
+            checked={formData.sauce === "marinara"}
           />
           <label htmlFor="marinara">Marinara</label>
         </div>
         <div className="radio">
           <input
-          onChange={handleChange}
+            onChange={handleChange}
             type="radio"
             name="sauce"
             value="alfredo"
-            checked={formData.sauce === 'alfredo'}
+            checked={formData.sauce === "alfredo"}
           />
           <label htmlFor="alfredo">Alfredo</label>
         </div>
         <div className="radio">
           <input
-          onChange={handleChange}
+            onChange={handleChange}
             type="radio"
             name="sauce"
             value="garlic"
-            checked={formData.sauce === 'garlic'}
+            checked={formData.sauce === "garlic"}
           />
           <label htmlFor="garlic">Garlic Sauce</label>
         </div>
         <div className="radio">
           <input
-          onChange={handleChange}
+            onChange={handleChange}
             type="radio"
             name="sauce"
             value="blackbean"
-            checked={formData.sauce === 'blackbean'}
+            checked={formData.sauce === "blackbean"}
           />
           <label htmlFor="blackbean">Black Bean Paste</label>
         </div>
         <div className="radio">
           <input
-          onChange={handleChange}
+            onChange={handleChange}
             type="radio"
             name="sauce"
             value="barbecue"
-            checked={formData.sauce === 'barbecue'}
+            checked={formData.sauce === "barbecue"}
           />
           <label htmlFor="barbecue">Tangy Barbecue</label>
+        </div>
+      </div>
+      {/* toppings checkboxes */}
+      <div style={{ display: "flex" }} className="toppingsWrapper">
+        <div
+          style={{ display: "flex", flexDirection: "column" }}
+          className="col"
+        >
+          <div className="toppingCheckBox">
+            <input
+              type="checkbox"
+              name="pepperoni"
+              value="pepperoni"
+              checked={formData.toppings.pepperoni}
+              onChange={handleToppings}
+            />
+            <label htmlFor="pepperoni">Pepperoni</label>
+          </div>
+          <div className="toppingCheckBox">
+            <input
+              type="checkbox"
+              name="pineapple"
+              value="pineapple"
+              checked={formData.toppings.pineapple}
+              onChange={handleToppings}
+            />
+            <label htmlFor="pineapple">Pineapple</label>
+          </div>
+          <div className="toppingCheckBox">
+            <input
+              type="checkbox"
+              name="olives"
+              value="olives"
+              checked={formData.toppings.olives}
+              onChange={handleToppings}
+            />
+            <label htmlFor="olives">Olives</label>
+          </div>
+          <div className="toppingCheckBox">
+            <input
+              type="checkbox"
+              name="bacon"
+              value="bacon"
+              checked={formData.toppings.bacon}
+              onChange={handleToppings}
+            />
+            <label htmlFor="bacon">Bacon</label>
+          </div>
+          <div className="toppingCheckBox">
+            <input
+              type="checkbox"
+              name="sausage"
+              value="sausage"
+              checked={formData.toppings.sausage}
+              onChange={handleToppings}
+            />
+            <label htmlFor="sausage">Sausage</label>
+          </div>
+          <div className="toppingCheckBox">
+            <input
+              type="checkbox"
+              name="hamburger"
+              value="hamburger"
+              checked={formData.toppings.hamburger}
+              onChange={handleToppings}
+            />
+            <label htmlFor="hamburger">Hamburger</label>
+          </div>
+          <div className="toppingCheckBox">
+            <input
+              type="checkbox"
+              name="chicken"
+              value="chicken"
+              checked={formData.toppings.chicken}
+              onChange={handleToppings}
+            />
+            <label htmlFor="chicken">Chicken</label>
+          </div>
+        </div>
+        <div
+          style={{ display: "flex", flexDirection: "column" }}
+          className="col"
+        >
+          <div className="toppingCheckBox">
+            <input
+              type="checkbox"
+              name="cheddar"
+              value="cheddar"
+              checked={formData.toppings.cheddar}
+              onChange={handleToppings}
+            />
+            <label htmlFor="cheddar">Cheddar</label>
+          </div>
+          <div className="toppingCheckBox">
+            <input
+              type="checkbox"
+              name="mozzarella"
+              value="mozzarella"
+              checked={formData.toppings.mozzarella}
+              onChange={handleToppings}
+            />
+            <label htmlFor="mozzarella">Mozzarella</label>
+          </div>
+          <div className="toppingCheckBox">
+            <input
+              type="checkbox"
+              name="sardines"
+              value="sardines"
+              checked={formData.toppings.sardines}
+              onChange={handleToppings}
+            />
+            <label htmlFor="sardines">Sardines</label>
+          </div>
+          <div className="toppingCheckBox">
+            <input
+              type="checkbox"
+              name="mushrooms"
+              value="mushrooms"
+              checked={formData.toppings.mushrooms}
+              onChange={handleToppings}
+            />
+            <label htmlFor="mushrooms">Mushrooms</label>
+          </div>
+          <div className="toppingCheckBox">
+            <input
+              type="checkbox"
+              name="onions"
+              value="onions"
+              checked={formData.toppings.onions}
+              onChange={handleToppings}
+            />
+            <label htmlFor="onions">Onions</label>
+          </div>
+          <div className="toppingCheckBox">
+            <input
+              type="checkbox"
+              name="peppers"
+              value="peppers"
+              checked={formData.toppings.peppers}
+              onChange={handleToppings}
+            />
+            <label htmlFor="peppers">Peppers</label>
+          </div>
+          <div className="toppingCheckBox">
+            <input
+              type="checkbox"
+              name="ham"
+              value="ham"
+              checked={formData.toppings.ham}
+              onChange={handleToppings}
+            />
+            <label htmlFor="ham">Ham</label>
+          </div>
         </div>
       </div>
     </form>
