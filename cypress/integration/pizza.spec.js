42d27bf5bd58d6ee32499dd903ca21c24f6c934a
Greg Brewton <gbrewton1980@gmail.com>
2021-04-19 00:41:27 -0600

    Can't get tests working

--- cypress/integration/pizza.spec.js
+++ cypress/integration/pizza.spec.js
@@ -1,8 +1,19 @@
+/* eslint-disable no-undef */
 describe('Lambda Eats', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:3000')
+        cy.visit('http://localhost:3000/pizza')
     })
     it ('sanity checks', () => {
         expect(42).to.equal(42);
     })
 })
+
+const textArea = () => cy.get(`textarea[name=specialInstructions]`)
+// add text to the box
+it('can add text to the box', () => {
+    textArea().should('exist')
+})
+
+// select multiple toppings
+
+// submit the form
